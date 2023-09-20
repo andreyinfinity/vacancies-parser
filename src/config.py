91864712e60572e5720a962ce67d2cbec635630f
Andrey Potapov <aapot@ya.ru>
2023-09-20 19:07:27 +0300

    изменен конфиг

--- src/config.py
+++ src/config.py
@@ -5,9 +5,10 @@ import os
 
 SJ_API_KEY = os.getenv('SJ_API_KEY')
 
-SJ_AREAS: str = os.path.join(os.path.dirname(__file__), "data", "sj-areas")
-SJ_VAC: str = os.path.join(os.path.dirname(__file__), "data", "sj-vac", "sj-vac")
+SJ_AREAS: str = os.path.join(os.path.dirname(__file__), "data", "sj-areas.json")
+SJ_VAC: str = os.path.join(os.path.dirname(__file__), "data", "sj-vac", "sj-vac.json")
 
-HH_AREAS: str = os.path.join(os.path.dirname(__file__), "data", "hh-areas")
-HH_VAC: str = os.path.join(os.path.dirname(__file__), "data", "hh-vac", "hh-vac")
+HH_AREAS: str = os.path.join(os.path.dirname(__file__), "data", "hh-areas.json")
+HH_VAC: str = os.path.join(os.path.dirname(__file__), "data", "hh-vac", "hh-vac.json")
 
+VAC: str = os.path.join(os.path.dirname(__file__), "data", "vac.json")
